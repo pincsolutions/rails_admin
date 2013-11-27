786a428951db52febae4216135a1646bbfae2682
nroose <nroose@gmail.com>
2013-11-26 20:23:22 -0800

    [#61493058]. Fixing rails admin export to abide by field order per https://github.com/sferik/rails_admin/commit/58817222e73e222fa9c1691040e099ce8531beb5.

--- lib/rails_admin/version.rb
+++ lib/rails_admin/version.rb
@@ -1,8 +1,8 @@
 module RailsAdmin
   class Version
     MAJOR = 2013 unless defined? MAJOR
-    MINOR = 1 unless defined? MINOR
-    PATCH = 29 unless defined? PATCH
+    MINOR = 11 unless defined? MINOR
+    PATCH = 26 unless defined? PATCH
     PRE = nil unless defined? PRE
 
     class << self
